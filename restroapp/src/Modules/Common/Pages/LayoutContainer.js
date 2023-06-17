2708ee2389e7646de7db793f5ea665591f00f67f
SG <sg19repos@gmail.com>
2023-06-17 15:44:46 +0530

    Moved RoutesTemp inside layoutContains to include useLocation

--- restroapp/src/Modules/Common/Pages/LayoutContainer.js
+++ restroapp/src/Modules/Common/Pages/LayoutContainer.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import { useLocation } from "react-router-dom";
+import CommonModuleRoutes from "./Routes/CommonModuleRoutes";
+import TopNavBar from "../../../Common/UIElements/atoms/TopNavigationBar";
+import RoutesTemp from "../../../Routes";
 
-const LayoutContainer = ({ children }) => {
+const LayoutContainer = () => {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const location = useLocation();
+  const updatedLocation = location.pathname || window.location.pathname;
+  useEffect(() => {
+    let routeElement = CommonModuleRoutes.find((element) => {
+      return element.path === updatedLocation?.split("/")?.[1];
+    });
+    setShowNavBar(routeElement?.showNavBar);
+  }, [updatedLocation]);
   return (
     <>
       <Grid
@@ -13,13 +26,23 @@ const LayoutContainer = ({ children }) => {
           // minHeight: !["/signup", "/login"].includes(history.location.pathname)
           // minHeight: footerPresent ? "calc(90vh)" : "calc(100vh)",
           minHeight: "calc(100vh)",
-        }}>
-        <Grid item xs={12}>
+        }}
+      >
+        {showNavBar && <TopNavBar sx={{ height: "10vh" }} />}
+        <Grid
+          item
+          xs={12}
+          style={{
+            height: showNavBar ? "90vh" : "100vh",
+            marginTop: showNavBar ? "1.8rem" : "",
+          }}
+        >
           <Box
             m={1}
             p={2}
             minHeight={"92%"}
-            className={"mainBorder plainBorder roundCorners"}>
+            className={"mainBorder plainBorder roundCorners"}
+          >
             {/*<PickupLocation />*/}
             {/*<Pincode />*/}
             {/*<CategoryList />*/}
@@ -35,7 +58,8 @@ const LayoutContainer = ({ children }) => {
             {/*<CarrierList />*/}
             {/*<ViewCarrier />*/}
             {/*<ModeSelection />*/}
-            {children}
+            <RoutesTemp />
+            {/*{children}*/}
             {/*{<OrderPreview />}*/}
           </Box>
         </Grid>
