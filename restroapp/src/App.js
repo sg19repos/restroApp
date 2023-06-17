3b3c8c885dd02dae60b4ddbc6c437e006902118d
SG <sg19repos@gmail.com>
2023-06-17 15:43:04 +0530

    Moved Router inside LayoutContainer to include useLocation

--- restroapp/src/App.js
+++ restroapp/src/App.js
@@ -1,33 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LayoutContainer from "./Modules/Common/Pages/LayoutContainer";
 import { BrowserRouter as Router } from "react-router-dom";
-import RoutesTemp from "./Routes";
-import TopNavBar from "./Common/UIElements/atoms/TopNavigationBar";
-import CommonModuleRoutes from "./Modules/Common/Pages/Routes/CommonModuleRoutes";
 
 function App() {
-  const [showNavBar, setShowNavBar] = useState(true);
-
-  useEffect(() => {
-    let routeElement = CommonModuleRoutes.find((element) => {
-      return element.path === window.location.pathname.split("/")?.[1];
-    });
-    setShowNavBar(routeElement?.showNavBar);
-  }, [window.location.pathname]);
-
   return (
     <Router>
-      <LayoutContainer>
-        {showNavBar && <TopNavBar sx={{ height: "10vh" }} />}
-        <div
-          style={{
-            height: showNavBar ? "90vh" : "100vh",
-            marginTop: showNavBar ? "1.8rem" : "",
-          }}
-        >
-          <RoutesTemp />
-        </div>
-      </LayoutContainer>
+      <LayoutContainer />
     </Router>
   );
 }
