a8fe6db759e012eda3842c3e9b898d938b5ee99b
SG <sg19repos@gmail.com>
2023-06-29 11:16:51 +0530

    Update Currency.jsx

--- restroapp/src/Common/UIElements/atoms/Currency.jsx
+++ restroapp/src/Common/UIElements/atoms/Currency.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { GlobalFont, GlobalFontColor } from "../../../Themes/GlobalThemes";
 
-export const Currency = ({ amount, fontSize }) => {
+export const Currency = ({ amount, fontSize, color = GlobalFontColor }) => {
   const currencySymbol = <span className={"currency-symbol"}>₹</span>;
   return (
-    <>
+    <div style={{ color: color }}>
       {currencySymbol}
-      <span style={{ fontSize: fontSize }} className={"list-item-font"}>
+      <span
+        style={{ fontSize: fontSize, ...GlobalFont }}
+        className={"list-item-font"}
+        color={color}
+      >
         {amount}
       </span>
-    </>
+    </div>
   );
 };
