888a6f1f07ed92e85ababae231e28b497f54dc8b
SG <sg19repos@gmail.com>
2023-06-30 13:42:57 +0530

    Created new totals page

--- restroapp/src/Common/UIElements/atoms/cartTotals.jsx
+++ restroapp/src/Common/UIElements/atoms/cartTotals.jsx
@@ -8,8 +8,8 @@ import {
 import { Currency } from "./Currency";
 
 const SubtotalTotalComponent = () => {
-  const subTotal = 100; // Example subtotal amount
-  const taxRate = 0.13; // Example tax rate
+  const subTotal = 100;
+  const taxRate = 0.13;
   const total = subTotal + subTotal * taxRate;
 
   return (
@@ -22,26 +22,24 @@ const SubtotalTotalComponent = () => {
         ...GlobalFontColor,
       }}
     >
-      {/* Subtotal section */}
       <Box sx={{ display: "flex", alignItems: "center", pb: 2 }}>
         <Typography variant="body1" sx={{ pr: 2, ...GlobalFont }}>
-          Subtotal:
+          Sub total
         </Typography>
-        <Box sx={{ flex: 1, borderBottom: "1px dotted #000" }}></Box>
+        {/*<Box sx={{ flex: 1, borderBottom: "1px dotted #000" }}></Box>*/}
+        <Box sx={{ flex: 1 }}>:</Box>
 
-        {/*${subTotal.toFixed(2)}*/}
         <span style={{ paddingLeft: "1rem" }}>
           <Currency amount={subTotal} />
         </span>
       </Box>
 
-      {/* Total section */}
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <Typography variant="body1" sx={{ pr: 2, ...GlobalFont }}>
-          Total:
+          Total
         </Typography>
-        <Box sx={{ flex: 1, borderBottom: "1px dotted #000" }}></Box>
-        {/*${total.toFixed(2)}*/}
+        {/*<Box sx={{ flex: 1, borderBottom: "1px dotted #000" }}></Box>*/}
+        <Box sx={{ flex: 1 }}>:</Box>
         <span style={{ paddingLeft: "1rem" }}>
           <Currency amount={total} />
         </span>
