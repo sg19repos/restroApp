f0a73350f0dc429f18a358c0a571396ab80a7ba2
SG <sg19repos@gmail.com>
2023-07-02 15:59:43 +0530

    Added item categories list

--- restroapp/src/Constants/constants.js
+++ restroapp/src/Constants/constants.js
@@ -46,3 +46,66 @@ export const MenuItems = [
     imageUrl: "idly",
   },
 ];
+
+export const itemCategories = {
+  biryani: {
+    vegBiryani: {
+      id: "vegBiryani",
+      title: "Veg biryani",
+      image: "vegBiryani",
+      price: "200",
+    },
+    chickenBiryani: {
+      id: "chickenBiryani",
+      title: "Chicken biryani",
+      image: "chickenBiryani",
+      price: "250",
+    },
+    muttonBiryani: {
+      id: "muttonBiryani",
+      title: "Mutton biryani",
+      image: "muttonBiryani",
+      price: "300",
+    },
+  },
+  dosa: {
+    plainDosa: {
+      id: "plainDosa",
+      title: "Plain Dosa",
+      image: "plainDosa",
+      price: "30",
+    },
+    masalaDosa: {
+      id: "masalaDosa",
+      title: "Masala Dosa",
+      image: "masalaDosa",
+      price: "40",
+    },
+    ravvaDosa: {
+      id: "ravvaDosa",
+      title: "Ravva Dosa",
+      image: "ravvaDosa",
+      price: "50",
+    },
+  },
+  idly: {
+    plainIdly: {
+      id: "plainIdly",
+      title: "Idly",
+      image: "plainIdly",
+      price: "30",
+    },
+    gheeIdly: {
+      id: "gheeIdly",
+      title: "Ghee Podi Idly",
+      image: "gheeIdly",
+      price: "40",
+    },
+    sambarIdly: {
+      id: "sambarIdly",
+      title: "Sambar Idly",
+      image: "sambarIdly",
+      price: "35",
+    },
+  },
+};
