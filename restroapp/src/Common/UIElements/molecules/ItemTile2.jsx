15772e4ef6d0c1c5c21fa1c1eeaa7edbda1d5621
SG <sg19repos@gmail.com>
2023-07-28 22:05:37 +0530

    Create ItemTile2.jsx

---
+++ restroapp/src/Common/UIElements/molecules/ItemTile2.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  Button,
+  Chip,
+  Avatar,
+  Divider,
+} from "@mui/material";
+import { QuantitySelection2 } from "../atoms/QuantitySelectionBtnGroup";
+import ImageComponent from "../Image";
+import { Currency } from "../atoms/Currency";
+import { GlobalFont } from "../../../Themes/GlobalThemes";
+import { Rating2 } from "../atoms/Rating2";
+import IconComponent from "../Icons";
+import DoneIcon from "@mui/icons-material/Done";
+import { useNavigate } from "react-router-dom";
+import { padding } from "@mui/system";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
+export const ItemTile2 = ({ element, cart, setCart }) => {
+  const navigate = useNavigate();
+  const handleItemClick = (itemId) => {
+    navigate(`/itemDetails?itemId=${itemId}`);
+  };
+  return (
+    <Paper elevation={2} sx={{ padding: "1rem", margin: "0.25rem" }}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={12} alignItems={"center"}>
+          <Grid
+            container
+            direction={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Grid item xs={12}>
+              <ImageComponent
+                name={element.imageUrl}
+                width={"95px"}
+                height={"100px"}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sx={{ paddingTop: "0.5rem" }}>
+          <Divider
+            sx={{ height: 1, backgroundColor: "gray", margin: "0.5rem 0" }}
+          />
+          <Grid container direction="column">
+            <Grid item xs={12}>
+              <Typography
+                sx={{
+                  ...GlobalFont,
+                  fontWeight: "800",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                }}
+                variant={"body2"}
+              >
+                {element.itemTitle}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography
+                sx={{
+                  ...GlobalFont,
+                  fontWeight: "200",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+                variant={"caption"}
+              >
+                {element.perPlatePieces}{" "}
+                {element.perPlatePieces > 1 ? "pieces" : "piece"}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} sx={{ fontWeight: "400", margin: "0.5rem 0rem" }}>
+            <Grid container direction={"row"}>
+              <Grid item xs={10}>
+                <Currency amount={element.itemPrice} fontSize={"1.25rem"} />
+              </Grid>
+              <Grid item xs={2}>
+                <FiberManualRecordIcon
+                  sx={{
+                    color: "green",
+                    fontSize: "0.5rem",
+                    border: "1px solid green",
+                    borderRadius: "20%",
+                    padding: "2px",
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            container
+            alignItems="center"
+            sx={{ margin: "0.5rem 0rem" }}
+          >
+            <QuantitySelection2 item={element} cart={cart} setCart={setCart} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
