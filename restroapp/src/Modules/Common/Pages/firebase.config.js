3d5ce2c0e1e921433aa98be1c7a2a742794c3764
SG <sg19repos@gmail.com>
2023-07-28 22:07:48 +0530

    Create firebase.config.js

---
+++ restroapp/src/Modules/Common/Pages/firebase.config.js
@@ -0,0 +1,24 @@
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJw1Cddd8NoFzAYwF7_ksqI3-ZPnrvSXk",
+  authDomain: "sgrestroapp.firebaseapp.com",
+  projectId: "sgrestroapp",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+*/
