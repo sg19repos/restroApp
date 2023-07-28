5b0f12be9450d3adaa15243c433fc26f193242b0
SG <sg19repos@gmail.com>
2023-07-28 22:03:20 +0530

    Update Rating2.jsx

--- restroapp/src/Common/UIElements/atoms/Rating2.jsx
+++ restroapp/src/Common/UIElements/atoms/Rating2.jsx
@@ -2,10 +2,15 @@ import * as React from "react";
 import Rating from "@mui/material/Rating";
 import { Typography } from "@mui/material";
 
-export const Rating2 = ({ rating, totalRatings, showTotals }) => {
+export const Rating2 = ({ rating, readOnly, totalRatings, showTotals }) => {
   return (
     <>
-      <Rating name="size-small" defaultValue={rating} size="small" />
+      <Rating
+        name="size-small"
+        readOnly={readOnly}
+        defaultValue={rating}
+        size="small"
+      />
       {showTotals && <Typography component="legend">{totalRatings}</Typography>}
     </>
   );
