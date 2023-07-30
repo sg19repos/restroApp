57f354f9b1ff9d6803361bc63c3fd3bdd2ca031c
SG <sg19repos@gmail.com>
2023-07-30 19:06:58 +0530

    Create mappings.js

---
+++ restroapp/src/Constants/mappings.js
@@ -0,0 +1,20 @@
+export const ItemCategoryMappings = {
+  IDLY: "idly",
+  VADA: "vada",
+  POORI: "poori",
+  DOSA: "dosa",
+  RICE: "rice",
+  BIRYANI: "biryani",
+  CURRY: "curry",
+};
+
+export const ItemTimeMappings = {
+  BREAKFAST: "breakfast",
+  SNACKS: "snacks",
+  DINNER: "dinner",
+};
+
+export const ItemVegNonVegMappings = {
+  VEG: "veg",
+  NONVEG: "nonVeg",
+};
